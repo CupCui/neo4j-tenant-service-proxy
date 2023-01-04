dc41c9afdfccd42511f78d43c9ffb34487c1d654
cuiguanghao <cuiguanghao@ultrapower.com.cn>
2023-01-04 16:48:52 +0800

    init project

---
+++ src/main/java/top/cupcupcui/Neo4jTenantServiceProxyApplication.java
@@ -0,0 +1,13 @@
+package top.cupcupcui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jTenantServiceProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jTenantServiceProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/cupcupcui/Neo4jTenantServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cupcupcui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jTenantServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
